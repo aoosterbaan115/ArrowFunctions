94f94bdbd4b56fbc4ac1673b5d552606ca2074ca
aoosterbaan115 <aoosterbaan115@yahoo.com>
2026-04-23 22:18:05 -0700

    Update reduce.test.js
    completed

--- reduce.test.js
+++ reduce.test.js
@@ -1,63 +1,43 @@
-describe("#extractValue", function() {
-  it("returns a new array with the value of each key in an array objects", function() {
-    var arr = [
-      { name: "Elie" },
-      { name: "Tim" },
-      { name: "Matt" },
-      { name: "Colt" }
-    ];
-    expect(extractValue(arr, "name")).toEqual(["Elie", "Tim", "Matt", "Colt"]);
-  });
-});
+// extractValue
+function extractValue(arr, key){
+  return arr.reduce(function(acc, obj){
+    acc.push(obj[key]);
+    return acc;
+  }, []);
+}
 
-describe("#vowelCount", function() {
-  it("returns an object with the keys as vowels and the values as the count", function() {
-    expect(vowelCount("elie")).toEqual({ e: 2, i: 1 });
-    expect(vowelCount("tim")).toEqual({ i: 1 });
-    expect(vowelCount("matt")).toEqual({ a: 1 });
-    expect(vowelCount("hmmm")).toEqual({});
-    expect(vowelCount("i am awesome and so are you")).toEqual({
-      i: 1,
-      a: 4,
-      e: 3,
-      o: 3,
-      u: 1
-    });
-  });
-});
 
-describe("#addKeyAndValue", function() {
-  var arr = [
-    { name: "Elie" },
-    { name: "Tim" },
-    { name: "Matt" },
-    { name: "Colt" }
-  ];
-  var updated = [
-    { title: "Instructor", name: "Elie" },
-    { title: "Instructor", name: "Tim" },
-    { title: "Instructor", name: "Matt" },
-    { title: "Instructor", name: "Colt" }
-  ];
-  it("adds a key and value to each object in an array of objects", function() {
-    expect(addKeyAndValue(arr, "title", "Instructor")).toEqual(updated);
-  });
-});
+// vowelCount
+function vowelCount(str){
+  const vowels = "aeiou";
 
-describe("#partition", function() {
-  function isEven(val) {
-    return val % 2 === 0;
-  }
-  function isLongerThanThreeCharacters(val) {
-    return val.length > 3;
-  }
-  var arr = [1, 2, 3, 4, 5, 6, 7, 8];
-  var names = ["Elie", "Colt", "Tim", "Matt"];
-  it("returns an array of arrays with the first subarray as values returning true from the callback", function() {
-    expect(partition(arr, isEven)).toEqual([[2, 4, 6, 8], [1, 3, 5, 7]]);
-    expect(partition(names, isLongerThanThreeCharacters)).toEqual([
-      ["Elie", "Colt", "Matt"],
-      ["Tim"]
-    ]);
-  });
-});
+  return str.toLowerCase().split("").reduce(function(acc, char){
+    if(vowels.includes(char)){
+      acc[char] = (acc[char] || 0) + 1;
+    }
+    return acc;
+  }, {});
+}
+
+
+// addKeyAndValue
+function addKeyAndValue(arr, key, value){
+  return arr.reduce(function(acc, obj){
+    obj[key] = value;
+    acc.push(obj);
+    return acc;
+  }, []);
+}
+
+
+// partition
+function partition(arr, callback){
+  return arr.reduce(function(acc, val){
+    if(callback(val)){
+      acc[0].push(val);
+    } else {
+      acc[1].push(val);
+    }
+    return acc;
+  }, [[], []]);
+}
